4f7b4de956e3e9c718eef75b13ab07e27853d5fa
jerryko570 <jerryko570@users.noreply.github.com>
2025-11-13 02:03:03 +0900

    solved: 10998 AxB (JavaScript)

---
+++ 10998_AxB.js
@@ -0,0 +1,7 @@
+// 백준 10998번: AxB
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+console.log(A * B);
